6b68899b81d930b418a321af9d9c0a50510fcd33
RaysonYT <nikolomoec@gmail.com>
2022-10-24 20:22:45 +0300

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Nikita Kolomoec on 24.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
