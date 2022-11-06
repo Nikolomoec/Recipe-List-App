a4b48fa27d4662bb50114fef53fc051c292f0a1b
RaysonYT <nikolomoec@gmail.com>
2022-11-06 20:55:52 +0200

    Add Environment Object, Add FeaturedView

---
+++ Recipe List App/Views/FeaturedView.swift
@@ -0,0 +1,23 @@
+//
+//  FeaturedView.swift
+//  Recipe List App
+//
+//  Created by Nikita Kolomoec on 06.11.2022.
+//
+
+import SwiftUI
+
+struct FeaturedView: View {
+    
+    @EnvironmentObject var model:RecipeModel
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeaturedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeaturedView()
+    }
+}
