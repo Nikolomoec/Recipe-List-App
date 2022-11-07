76fb60a0257d571947fbcbb82c49ad957b80e333
RaysonYT <nikolomoec@gmail.com>
2022-11-07 22:51:44 +0200

    * Modify Featured View

--- Recipe List App/Views/RecipeListView.swift
+++ Recipe List App/Views/RecipeListView.swift
@@ -38,5 +38,6 @@ struct RecipeListView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
+            .environmentObject(RecipeModel())
     }
 }
